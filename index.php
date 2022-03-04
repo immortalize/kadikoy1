1ad5fa2709caa1e7097c9dc2efb5875212af7b39
611kemal611 <100923224+611kemal611@users.noreply.github.com>
2022-03-04 12:58:09 +0300

    kdv hesaplayıcı
    Sizin için hesaplama yapabilirim

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-echo "hocam ara verelim mi ?";
+?php
+ 
+$fiyat=85;
+ 
+$kdv=$fiyat*18/100;
+ 
+$toplam=$fiyat+$kdv;
+ 
+echo "Tutar=".$toplam."<br>";
+ 
+if($toplam>=100)
+       echo "Kargo Bedava!";
+ 
 ?>
